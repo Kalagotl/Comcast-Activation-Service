40375c9ce8cbad7e4e2cfd9a1873be47fb3361ff
Dev1 <shekhardpd@gmail.com>
2024-06-22 09:24:42 +0530

    Commited

---
+++ Comcast-Activation-Service/src/main/java/comcast/activation/ComcastActivationServiceApplication.java
@@ -0,0 +1,13 @@
+package comcast.activation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComcastActivationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComcastActivationServiceApplication.class, args);
+	}
+
+}
---
+++ Comcast-Activation-Service/src/test/java/comcast/activation/ComcastActivationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package comcast.activation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComcastActivationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
